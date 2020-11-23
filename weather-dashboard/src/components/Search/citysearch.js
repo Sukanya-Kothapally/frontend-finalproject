774fb8b967006b524485f6cd8ef42720f1fc00af
jethah2@pdx.edu <jethah2@pdx.edu>
2020-11-23 12:18:53 -0800

    Handled block feature and added line chart component

--- weather-dashboard/src/components/Search/citysearch.js
+++ weather-dashboard/src/components/Search/citysearch.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import axios from "axios";
 import CurrentWeather from "../Currentweather/current";
+import LineChart from "../Charts/LineChart";
 import "./citysearchstyle.css";
 
 class SearchCity extends Component {
   state = {
+    name: "",
     search: "",
     latitude: undefined,
     longitude : undefined,
@@ -19,12 +21,13 @@ class SearchCity extends Component {
         //console.log(position);
         axios.get("https://api.openweathermap.org/data/2.5/weather?lat=" + lat +"&lon=" +lon +"&units=imperial&appid=61d5f8577e9dc21f1a56b94167a17bf8")
        .then((response) => {
-          //const name = response.data.name;
+          const name = response.data.name;
           const lon = response.data.coord.lon;
           const lat = response.data.coord.lat;
           currentComponent.setState({
             latitude: lat,
             longitude: lon,
+            name: name,
           });
         });
       });
@@ -33,16 +36,20 @@ class SearchCity extends Component {
   
   OnClickCitySearch = (event) => {
     event.preventDefault();
-    axios.get("https://api.openweathermap.org/data/2.5/weather?q=" + this.state.search + "&units=imperial&appid=61d5f8577e9dc21f1a56b94167a17bf8")
+    axios.get
+    (
+      "https://api.openweathermap.org/data/2.5/weather?q=" + this.state.search + "&units=imperial&appid=61d5f8577e9dc21f1a56b94167a17bf8"
+    )
     .then((response) => {
       if (response.status === 400 || response.status === 500) {
         throw new Error(response.statusText);
       }
-     // const name = response.data.name;
+      const name = response.data.name;
       const lon = response.data.coord.lon;
       const lat = response.data.coord.lat;
       //console.log(response);
       this.setState({
+        name: name,
         latitude: lat,
         longitude: lon,
       });
@@ -58,6 +65,8 @@ class SearchCity extends Component {
   };
 
   render() {
+    const { name } = this.state;
+    if(name){
     return (
       <div className="container">
         <div className="row">
@@ -71,19 +80,26 @@ class SearchCity extends Component {
                 placeholder="Search by City Name"
                 value={this.value}
                 onChange={this.onClickCityChange}
-              />
-              <div className="input-group-append">
+              />&nbsp;
                 <button
                   type="submit"
                   className="btnSearch btn "
                   onClick={this.OnClickCitySearch}>
                   Search
                 </button>
-              </div>
             </div>
           </div>
-          <div className="col-10">
+          </div>
+          <br /><br />
+          <div className="row">
+          <div className="col-6">
               <CurrentWeather
+                lat ={this.state.latitude}
+                lon ={this.state.longitude}
+              />
+          </div>
+          <div className="col-6">
+              <LineChart
                 lat ={this.state.latitude}
                 lon ={this.state.longitude}
               />
@@ -91,6 +107,34 @@ class SearchCity extends Component {
         </div>
       </div>
     );
+    }
+    else{
+      return(
+      <div className="container">
+      <div className="row">
+      <div className=" ml-auto col-md-6  col-sm-6  ">
+        <div className=" input-group row">
+          <input
+            className="inputSearch form-control"
+            type="text"
+            name="city"
+            id="city"
+            placeholder="Search by City Name"
+            value={this.value}
+            onChange={this.onClickCityChange}
+          />&nbsp;
+            <button
+              type="submit"
+              className="btnSearch btn "
+              onClick={this.OnClickCitySearch}>
+              Search
+            </button>
+        </div>
+      </div>
+      </div>
+      </div>
+      );
+    }
   }
 }
 
