ca2d108eb66d5bf47f79b9b962f88c3ed9dbfc6f
jethah2@pdx.edu <jethah2@pdx.edu>
2020-11-25 16:42:23 -0800

    Modified app.js completely to add navbar

--- weather-dashboard/src/App.js
+++ weather-dashboard/src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from "./About";
+import Header from "./header";
 import Homepage from "./view/home"
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Router>
+        <Header />
+        <Route path="/" exact component={Homepage} />
+        <Route path="/About" component={About} />
+        </Router>
     </div>
   );
 }
