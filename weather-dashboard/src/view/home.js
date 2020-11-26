3c540c520057517c964c73fffa4b99abd17da493
jethah2@pdx.edu <jethah2@pdx.edu>
2020-11-25 17:01:36 -0800

    Removed the header component loading

--- weather-dashboard/src/view/home.js
+++ weather-dashboard/src/view/home.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Header from "../components/Header/header";
 import SearchCity from "../components/Search/citysearch";
 import Footer from "../components/Footer/footer";
 
 function Homepage() {
   return (
     <div className="home">
-      <Header />
       <SearchCity />
       <Footer />
     </div>
